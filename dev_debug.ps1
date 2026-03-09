2635e36412cbe45c52d5b8eca1659c7e54b7281f
nurwendi <hudinurwendi@gmail.com>
2026-03-10 00:21:54 +0700

    chore: cleanup unused mobile files and platform scripts

--- dev_debug.ps1
+++
@@ -1,3 +0,0 @@
-$nodePath = "C:\Program Files\nodejs\node.exe"
-$nextPath = "f:\iki\netflowpro\node_modules\next\dist\bin\next"
-Start-Process -FilePath $nodePath -ArgumentList "$nextPath dev" -RedirectStandardOutput "startup.log" -RedirectStandardError "startup.log" -NoNewWindow -PassThru
