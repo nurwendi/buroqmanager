3fb9d8fc529d93436953a33b1a264bde9a657128
nurwendi <hudinurwendi@gmail.com>
2026-09-25 13:24:20 +0700

    chore: adjust pm2 memory limits safely for 8gb server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,9 @@ module.exports = {
       args: 'start -p 2000',
       instances: 'max',
       exec_mode: 'cluster',
-      node_args: '--max-old-space-size=4096', // Mengalokasikan hingga 4GB RAM per Core jika tersedia
+      node_args: '--max-old-space-size=1024', // Mengalokasikan max 1GB RAM per Core (Total 4GB untuk 4 Core)
       watch: false,
-      max_memory_restart: '3G', // Batas aman restart dinaikkan menjadi 3GB
+      max_memory_restart: '1G', // Restart otomatis jika memori tembus 1GB per-core agar tidak menghabiskan RAM server
       env: {
         NODE_ENV: 'production'
       }
