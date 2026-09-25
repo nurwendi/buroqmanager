5599508c740b38ae188fdf04b14bc6a6c32f60bf
nurwendi <hudinurwendi@gmail.com>
2026-09-25 13:21:54 +0700

    perf: increase nodejs heap memory limit for pm2 cluster

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,9 @@ module.exports = {
       args: 'start -p 2000',
       instances: 'max',
       exec_mode: 'cluster',
+      node_args: '--max-old-space-size=4096', // Mengalokasikan hingga 4GB RAM per Core jika tersedia
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: '3G', // Batas aman restart dinaikkan menjadi 3GB
       env: {
         NODE_ENV: 'production'
       }
